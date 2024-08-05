afe6e0e88a196af3485341f8c363736e982c404a
BortolatoYan <root@hosting.waredatacenter.com.br>
2024-08-05 19:17:36 -0300

    start-transform

--- dags/gcs_download_file_dag.py
+++ dags/gcs_download_file_dag.py
@@ -1,33 +1,51 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.google.cloud.transfers.gcs_to_local import GCSToLocalFilesystemOperator
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
 import os
 
-
 def get_file_name(**kwargs):
     current_time = datetime.now() - timedelta(hours=1)  # Subtrai uma hora da hora atual
-    file_name = f'Picking/picking-{current_time.strftime("%Y-%m-%d-%H.csv")}'
+    file_name = f'picking-{current_time.strftime("%Y-%m-%d-%H.csv")}'
     kwargs['ti'].xcom_push(key='file_name', value=file_name)  # Passa o nome do arquivo para o XCom
 
 def download_file(**kwargs):
     ti = kwargs['ti']
     file_name = ti.xcom_pull(task_ids='get_file_name', key='file_name')
+    print('filename :D', file_name)
 
-    local_file_path = os.path.join('/opt/airflow/data_lake/Picking', os.path.basename(file_name))
+    local_file_path = os.path.join('/opt/airflow/data_lake/Bronze/Picking', file_name)
     
     # Cria uma instância do GCSToLocalFilesystemOperator com o nome do arquivo
     download_task = GCSToLocalFilesystemOperator(
         task_id='download_file',
         bucket='wms-extract-hour',
-        object_name=file_name,
+        object_name=f'Bronze/Picking/{file_name}',
         filename=local_file_path,
         gcp_conn_id='google-cloud-storage',
         trigger_rule='all_success'  # Executa apenas se as tarefas anteriores tiverem sucesso
     )
     # Execute a tarefa criada
     download_task.execute(context=kwargs)
+
+def upload_to_gcs(**kwargs):
+    ti = kwargs['ti']
+    file_name = ti.xcom_pull(task_ids='get_file_name', key='file_name')
+    local_file_path = os.path.join('/opt/airflow/data_lake/Silver/Picking', file_name)
+    
+    # Cria uma instância do LocalFilesystemToGCSOperator com o nome do arquivo
+    upload_task = LocalFilesystemToGCSOperator(
+        task_id='upload_to_gcs_task',
+        bucket='wms-extract-hour',
+        dst=f'Silver/Picking/{file_name}',
+        src=local_file_path,
+        gcp_conn_id='google-cloud-storage'
+    )
+    # Execute a tarefa criada
+    upload_task.execute(context=kwargs)
 
 # Definição do DAG
 default_args = {
@@ -37,7 +55,7 @@ default_args = {
 }
 
 with DAG(
-    'gcs_download_file',
+    'gcs_download_and_upload_file',
     default_args=default_args,
     schedule_interval='@hourly',  # Executa uma vez por hora
     catchup=False
@@ -55,4 +73,16 @@ with DAG(
         provide_context=True
     )
 
-    get_file_name_task >> download_file_task
+    transform_print_task = BashOperator(
+        task_id='transform_print_task',
+        bash_command="python3 /opt/airflow/scripts/transform_picking.py {{ ti.xcom_pull(task_ids='get_file_name', key='file_name') }}"
+    )
+
+    upload_to_gcs_task = PythonOperator(
+        task_id='upload_to_gcs_task',
+        python_callable=upload_to_gcs,
+        provide_context=True
+    )
+
+    # Definição das dependências
+    get_file_name_task >> download_file_task >> transform_print_task >> upload_to_gcs_task
